119f6ae90c072b1c576a8884da6bd376fb5bf82c
George Kaye <georgejkaye@gmail.com>
2026-06-28 22:40:55 +0100

    fix: attempt to make work

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 
-echo "about to ls"
-ls -R ~/.opam
-echo "did the ls"
-opam init --disable-sandboxing -y
+opam init --bare --disable-sandboxing -y
 opam switch create .--jobs=8 --locked
 
 eval $(opam env)
