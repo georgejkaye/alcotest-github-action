d544cc5e355dc134797200d8f96d299e1c374547
George Kaye <georgejkaye@gmail.com>
2026-06-30 22:08:34 +0100

    feat: remove colour from alcotest logs

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 PROJECT_DIR=$1
 
 pushd $PROJECT_DIR > /dev/null
-RUNTEST_OUTPUT=$(opam exec -- dune runtest 3>&2 2>&1 1>&3)
+RUNTEST_OUTPUT=$(ALCOTEST_COLOR=never opam exec -- dune runtest 3>&2 2>&1 1>&3)
 popd > /dev/null
 
 dune exec alcotest_action "$RUNTEST_OUTPUT"
