8fec1651d81f242ec328d2dc66911823d025c17f
George Kaye <georgejkaye@gmail.com>
2026-07-07 21:17:01 +0100

    feat: add output path argument to entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
 PROJECT_DIR=$1
+OUTPUT_PATH=$2
 
 pushd $PROJECT_DIR > /dev/null
 RUNTEST_OUTPUT=$(ALCOTEST_COLOR=never opam exec -- dune runtest 3>&2 2>&1 1>&3)
 popd > /dev/null
 
-dune exec alcotest_action "$RUNTEST_OUTPUT"
+dune exec alcotest_action "$OUTPUT_PATH" "$RUNTEST_OUTPUT"
