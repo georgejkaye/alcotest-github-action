3d09f1793396252238b9ab8ebfa337b73c671fc0
George Kaye <georgejkaye@gmail.com>
2026-07-08 20:46:20 +0100

    feat: build alcotest for use in action

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,8 @@
 PROJECT_DIR=$1
 OUTPUT_PATH=$2
 
-opam init
-
 pushd $PROJECT_DIR > /dev/null
 RUNTEST_OUTPUT=$(ALCOTEST_COLOR=never opam exec -- dune runtest 3>&2 2>&1 1>&3)
 popd > /dev/null
 
-dune exec alcotest_action "$OUTPUT_PATH" "$RUNTEST_OUTPUT"
+./_build/default/bin/main.exe "$OUTPUT_PATH" "$RUNTEST_OUTPUT"
