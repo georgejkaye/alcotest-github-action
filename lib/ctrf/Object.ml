1f04074b6b8d112b6276bda9c350665a654a7992
George Kaye <georgejkaye@gmail.com>
2026-08-11 22:20:53 +0100

    feat: add empty object

--- lib/ctrf/Object.ml
+++ lib/ctrf/Object.ml
@@ -8,3 +8,13 @@ module type T = sig
   val to_yojson : t -> Yojson.Safe.t
   val of_yojson : Yojson.Safe.t -> (t, string) result
 end
+
+module Empty : T = struct
+  type t =
+    (unit
+    [@to_yojson fun _ -> `Assoc []]
+    [@of_yojson function `Assoc [] -> Ok () | _ -> Error "Object.Empty.t"])
+  [@@deriving show, yojson]
+
+  let v = ()
+end
