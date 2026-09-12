286b2f6353de335449c81a67fcf8e82416ba37cf
George Kaye <georgejkaye@gmail.com>
2026-09-12 21:02:11 +0100

    refactor: move alcotest tests to parser tests

--- test/Test.ml
+++ test/Test.ml
@@ -1,3 +1,3 @@
 let () =
   Alcotest.run "Alcotest_action"
-    [ Alcotest_tests.Headline_tests.tests; Alcotest_tests.Output_tests.tests ]
+    [ Parser_tests.Headline_tests.tests; Parser_tests.Output_tests.tests ]
