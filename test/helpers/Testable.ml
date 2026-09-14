398062ce9567b49cd6a7bce931e89264645db763
George Kaye <georgejkaye@gmail.com>
2026-09-14 22:04:01 +0100

    test: add report to testable

--- test/helpers/Testable.ml
+++ test/helpers/Testable.ml
@@ -17,3 +17,4 @@ let either at bt =
 
 let list at = Alcotest.(list at)
 let headline = Alcotest.testable Parser.Headline.pp Parser.Headline.equal
+let report = Alcotest.testable Parser.Report.pp Parser.Report.equal
