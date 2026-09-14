57e4d43a1969d2a06deb5493697db8c33f51072f
George Kaye <georgejkaye@gmail.com>
2026-09-14 21:26:56 +0100

    test: add paths function test

--- test/Test.ml
+++ test/Test.ml
@@ -1,3 +1,7 @@
 let () =
   Alcotest.run "Alcotest_action"
-    [ Parser_tests.Headline_tests.tests; Parser_tests.Output_tests.tests ]
+    [
+      Parser_tests.Headline_tests.tests;
+      Parser_tests.Output_tests.tests;
+      Parser_tests.Paths_tests.tests;
+    ]
