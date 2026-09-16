462ddfc29a00f2c41c1e5aaf242a3e118f8532bf
George Kaye <georgejkaye@gmail.com>
2026-09-16 18:34:04 +0100

    refactor: rename alcotest module to parser

--- bin/main.ml
+++ bin/main.ml
@@ -10,30 +10,30 @@ let run ~alcotest_input_path ~build_root_dir ~ctrf_output_path ~start_timestamp
   match File.read_file alcotest_input_path with
   | Second msg -> failwith msg
   | First test_output -> (
-      match Alcotest.Output.get_id test_output with
+      match Parser.Output.get_id test_output with
       | Second msg -> failwith msg
       | First test_run_id -> (
           let test_run_name =
-            match Alcotest.Output.get_name test_output with
+            match Parser.Output.get_name test_output with
             | First name -> name
             | Second _ -> "Test run"
           in
           match
-            Alcotest.Paths.get_test_logs_root_path ~test_root_dir:build_root_dir
+            Parser.Paths.get_test_logs_root_path ~test_root_dir:build_root_dir
               test_run_id
           with
           | Second err -> failwith err
           | First test_log_root ->
               let test_headlines =
-                Alcotest.Output.get_test_headlines test_output
+                Parser.Output.get_test_headlines test_output
               in
               let test_report =
-                Alcotest.Report.of_test_headlines ~name:test_run_name
+                Parser.Report.of_test_headlines ~name:test_run_name
                   ~id:test_run_id ~start_timestamp ~end_timestamp
                   ~version:alcotest_version test_headlines test_log_root
               in
               File.write_file ctrf_output_path
-                (Alcotest.Report.to_ctrf test_report
+                (Parser.Report.to_ctrf test_report
                 |> Root.to_yojson
                 |> Yojson.remove_nulls
                 |> Yojson.Safe.to_string)))
@@ -67,7 +67,7 @@ let datetime_of_string_arg_exn ?(arg_name = "") arg =
            %{Exn.to_string exn}"]
 
 let command =
-  Command.basic ~summary:"Process Alcotest output"
+  Command.basic ~summary:"Process Parser.output"
     ~readme:(fun () -> "Todo")
     (let%map_open.Command alcotest_input_path_string =
        anon ("alcotest_input_path" %: string)
