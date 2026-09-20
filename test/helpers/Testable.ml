ade5bfc499a2797b9b9e9da078b2436278d989d2
George Kaye <georgejkaye@gmail.com>
2026-09-20 21:24:54 +0100

    test: add more types to testable helpers

--- test/helpers/Testable.ml
+++ test/helpers/Testable.ml
@@ -3,6 +3,11 @@ open! Core
 
 let string = Alcotest.string
 let int = Alcotest.int
+
+let exn =
+  Alcotest.testable Exn.pp (fun a b ->
+      Sexp.equal (Exn.sexp_of_t a) (Exn.sexp_of_t b))
+
 let fpath = Alcotest.testable Fpath.pp Fpath.equal
 
 let either at bt =
@@ -13,6 +18,16 @@ let either at bt =
       match (a, b) with
       | First a, First b -> (Alcotest.equal at) a b
       | Second a, Second b -> (Alcotest.equal bt) a b
+      | _ -> false)
+
+let result at bt =
+  Alcotest.testable
+    (fun ppf -> function
+      | Ok a -> (Alcotest.pp at) ppf a | Error b -> (Alcotest.pp bt) ppf b)
+    (fun a b ->
+      match (a, b) with
+      | Ok a, Ok b -> (Alcotest.equal at) a b
+      | Error a, Error b -> (Alcotest.equal bt) a b
       | _ -> false)
 
 let list at = Alcotest.(list at)
@@ -31,3 +46,5 @@ end
 module Root = Ctrf.Root.MakeWithNoExtras (Ctrf.Object.Empty) (Ctrf.Object.Empty)
 
 let root_with_no_extras = Alcotest.testable Root.pp Root.equal
+let time_float_unix = Alcotest.testable Time_float_unix.pp Time_float_unix.equal
+let yojson = Alcotest.testable Yojson.Safe.pp Yojson.Safe.equal
